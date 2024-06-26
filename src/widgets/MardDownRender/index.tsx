0f593375f53d132f0128ebb2412e66d489efa169
Stepan S <dumn2011@gmail.com>
2024-06-26 22:57:54 +0300

    feat: export paragraphs with align

--- src/widgets/MardDownRender/index.tsx
+++ src/widgets/MardDownRender/index.tsx
@@ -1,15 +1,42 @@
 import Markdown from "react-markdown";
 import { InitialConfigType } from "@lexical/react/LexicalComposer";
 import { INITIAL_TEXT, NODES, theme } from "../../shared/constants";
-import { createEditor } from "lexical";
+import { $isParagraphNode, createEditor, ParagraphNode } from "lexical";
 import { CreateEditorArgs } from "lexical/LexicalEditor";
 import { useStoreContext } from "../../contexts/StoreContext";
 import { useEffect, useState } from "react";
 import { createHeadlessEditor } from "@lexical/headless";
-import { $convertToMarkdownString, TRANSFORMERS } from "@lexical/markdown";
+import {
+  $convertToMarkdownString,
+  ElementTransformer,
+  TRANSFORMERS,
+} from "@lexical/markdown";
 import { LinkNode } from "@lexical/link";
+import { Flex } from "antd";
+import rehypeRaw from "rehype-raw";
 
 const onError = (error: Error) => console.error(error);
+
+export const PARAGRAPH: ElementTransformer = {
+  dependencies: [ParagraphNode],
+  export: (node, exportChildren) => {
+    if (!$isParagraphNode(node)) {
+      return null;
+    }
+
+    const lines = exportChildren(node);
+    console.log("lines: ", lines);
+    // const output = [];
+    // for (const line of lines) {
+    //   output.push('> ' + line);
+    // }
+    // return output.join('\n');
+    const out = `<p style="text-align: ${node.getFormatType() || "left"}"> ${lines} </p>`;
+
+    return out;
+  },
+  type: "element",
+};
 
 export const MardDownRender = () => {
   const { blocks } = useStoreContext();
@@ -34,11 +61,15 @@ export const MardDownRender = () => {
       },
     );
     editor.update(() => {
-      const markdown = $convertToMarkdownString(TRANSFORMERS);
+      const markdown = $convertToMarkdownString([...TRANSFORMERS, PARAGRAPH]);
       console.log("markdown: ", markdown);
       setMarkDown(markdown);
     });
   }, [blocks]);
 
-  return <Markdown>{markddown}</Markdown>;
+  return (
+    <Flex vertical>
+      <Markdown rehypePlugins={[rehypeRaw]}>{markddown}</Markdown>
+    </Flex>
+  );
 };
