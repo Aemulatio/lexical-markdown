9fadd51ba497e73c2967f685cc8ce3995bbcda27
Stepan S <dumn2011@gmail.com>
2024-06-26 22:32:08 +0300

    feat: add ability to align to left or right

--- src/widgets/Editor/ui/Toolbar/index.tsx
+++ src/widgets/Editor/ui/Toolbar/index.tsx
@@ -6,6 +6,7 @@ import {
   TEXT_FORMAT_TRANSFORMERS,
   TEXT_MATCH_TRANSFORMERS,
 } from "@lexical/markdown";
+import { FORMAT_ELEMENT_COMMAND } from "lexical";
 
 export const Toolbar = () => {
   const [editor] = useLexicalComposerContext();
@@ -13,19 +14,23 @@ export const Toolbar = () => {
   const handleMarkDown = () => {
     editor.getEditorState().read(() => {
       // const state = editor.getEditorState()
-      const markdown = $convertToMarkdownString([
-        ELEMENT_TRANSFORMERS,
-        TEXT_FORMAT_TRANSFORMERS,
-        TEXT_MATCH_TRANSFORMERS,
-      ]);
+      const markdown = $convertToMarkdownString();
       console.log(markdown);
     });
+  };
+
+  const handleToLeft = () => {
+    editor.dispatchCommand(FORMAT_ELEMENT_COMMAND, "left");
+  };
+  const handleToRight = () => {
+    editor.dispatchCommand(FORMAT_ELEMENT_COMMAND, "right");
   };
 
   return (
     <Flex gap={"10px"} justify={"space-between"}>
       <Button onClick={handleMarkDown}>Markdown</Button>
-      <Button>Link</Button>
+      <Button onClick={handleToLeft}>To left</Button>
+      <Button onClick={handleToRight}>To right</Button>
     </Flex>
   );
 };
